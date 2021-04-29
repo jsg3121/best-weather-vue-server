4f3fa17b77a9f825b7be3bb61f5e4857b9407061
jsg3121 <xodm95@gmail.com>
2021-04-29 18:14:57 +0900

    FIX(time out) : Time out Error

--- src/api/src/koreaWeather/src/korea.weather.api.ts
+++ src/api/src/koreaWeather/src/korea.weather.api.ts
@@ -73,11 +73,15 @@ export const threeHoursWeather: dailyWeatherRequestProps = async (data) => {
   const WSD: threeHourWeatherOutput[] = [];
 
   const time = parseInt(changDateFormThreeHoursTime(), 10);
-  const request = await axios.get(`http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst?serviceKey=${APIKEY}&numOfRows=180&pageNo=1&dataType=json&base_date=${BASE_DATE}&base_time=${time}&nx=${nx ? nx : 60}&ny=${ny ? ny : 127}`).then((res) => {
-    return res.data.response.body;
-  });
-
-  const res = request.items.item;
+  const res = await axios
+    .get(`http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst?serviceKey=${APIKEY}&numOfRows=180&pageNo=1&dataType=json&base_date=${BASE_DATE}&base_time=${time}&nx=${nx ? nx : 60}&ny=${ny ? ny : 127}`)
+    .then((res) => {
+      return res.data.response.body.items.item;
+    })
+    .catch((e) => {
+      console.log(e);
+      return e;
+    });
 
   res.map((item: threeHourWeatherOption) => {
     switch (item.category) {
