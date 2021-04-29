64d170f788a10b52e36fad9d2a0848e390c79e44
jsg3121 <xodm95@gmail.com>
2021-04-29 16:21:22 +0900

    FEAT(err) : response test

--- src/service/src/korea.weather.ts
+++ src/service/src/korea.weather.ts
@@ -7,21 +7,33 @@ export const weatherKorea = () => {
     res.end();
   });
   app.get("/api/daily", async (req, res) => {
+    console.log("--------------------------------------------------");
+    console.log("daily");
+    console.log("--------------------------------------------------");
     const data = await getDailyWeather(req.query);
     res.send(data);
     res.end();
   });
   app.get("/api/weekly", async (req, res) => {
+    console.log("--------------------------------------------------");
+    console.log("weekly");
+    console.log("--------------------------------------------------");
     const data = await getMaxMinTemperature(req.query);
     res.send(data);
     res.end();
   });
   app.get("/api/threeHours", async (req, res) => {
+    console.log("--------------------------------------------------");
+    console.log("threeHours");
+    console.log("--------------------------------------------------");
     const data = await threeHoursWeather(req.query);
     res.send(data);
     res.end();
   });
   app.get("/api/livingInformation", async (_, res) => {
+    console.log("--------------------------------------------------");
+    console.log("livingInformation");
+    console.log("--------------------------------------------------");
     const data = await livingInfomation();
     res.send(data);
     res.end();
