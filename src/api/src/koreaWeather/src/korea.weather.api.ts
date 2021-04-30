f42edcf682cb7942b825f72cc7fe06ba656ca0f9
jsg3121 <xodm95@gmail.com>
2021-04-30 14:33:03 +0900

    TEST(time) : timezone

--- src/api/src/koreaWeather/src/korea.weather.api.ts
+++ src/api/src/koreaWeather/src/korea.weather.api.ts
@@ -75,6 +75,8 @@ export const threeHoursWeather: dailyWeatherRequestProps = async (data) => {
 
   const time = parseInt(changDateFormThreeHoursTime(), 10);
   const res = await axios.get(`http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst?serviceKey=${APIKEY}&numOfRows=40&pageNo=1&dataType=json&base_date=${BASE_DATE}&base_time=${time}&nx=${nx ? nx : 60}&ny=${ny ? ny : 127}`).then((res) => {
+    console.log(time);
+    console.log(res);
     return res.data.response.body.items.item;
   });
 
