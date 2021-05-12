a0a0a9080ddfcac30cd516c5677e3f2663817a75
LAPTOP-LUU9NKG5\quber <sgjang@quber.net>
2021-05-12 13:47:58 +0900

    FEAT(heroku) : 다음날 API 무한 로딩현상 확인 테스트

--- src/cron/src/serverWake.ts
+++ src/cron/src/serverWake.ts
@@ -2,7 +2,8 @@ import { app } from "~/server";
 
 export const serverWake = () => {
   setInterval(() => {
-    return app.get("https://best-weather-vue.herokuapp.com/", async (_, res) => {
+    app.get("https://best-weather-vue.herokuapp.com/", async (_, res) => {
+      console.log("...ing");
       res.send("server start");
       res.end();
     });
