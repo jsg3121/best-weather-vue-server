4413a5c4e8b447772b6472c17ee81fefadbf9b3f
LAPTOP-LUU9NKG5\quber <sgjang@quber.net>
2021-05-13 10:39:52 +0900

    FIX(day.js) : dayJs timeZone change

--- src/common/src/date.ts
+++ src/common/src/date.ts
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 import timezone from "dayjs/plugin/timezone";
-
+import utc from "dayjs/plugin/utc";
+dayjs.extend(utc);
 dayjs.extend(timezone);
 const TODAY = dayjs().tz("Asia/Seoul");
 
