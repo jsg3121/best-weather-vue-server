f10ceb619cdaf53e46d3133d469d2e8526a1a902
jsg3121 <xodm95@gmail.com>
2021-07-20 16:08:45 +0900

    FEAT(front) : Front View데모 업로드

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { runServer } from "./server";
 import cors from "cors";
 import express from "express";
+import path from "path";
+
 export const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(process.cwd(), "public")));
 
 runServer();
