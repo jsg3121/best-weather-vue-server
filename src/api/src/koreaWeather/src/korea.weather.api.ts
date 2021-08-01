76c4269613456841fd898018ebf81cab7d67a390
jsg3121 <xodm95@gmail.com>
2021-08-01 19:46:06 +0900

    FIX(dailyWeather) : return값 수정

--- src/api/src/koreaWeather/src/korea.weather.api.ts
+++ src/api/src/koreaWeather/src/korea.weather.api.ts
@@ -9,12 +9,10 @@ export const getDailyWeather: GeolocationProps = async (data) => {
 
   const { nx, ny } = data;
 
-  const res: object[] = [];
-
   const nowTemperatures: Promise<AxiosResponse<resultDailyDataProps>> = await axios.get(`http://apis.data.go.kr/1360000/VilageFcstInfoService/getUltraSrtNcst?serviceKey=${KOREA_WEATHER_API_KEY}&numOfRows=10&pageNo=1&dataType=json&base_date=${BASE_DATE}&base_time=${BASE_TIME}&nx=${nx ? nx : 60}&ny=${ny ? ny : 127}`).then((res) => {
     const result = res.data.response.body.items.item;
     return result.filter((item: resultDailyDataProps) => {
-      return item.category === "T1H" || item.category === "REH" || item.category === "RN1";
+      return item.category === "T1H" || item.category === "REH" || item.category === "RN1" || item.category === "VEC" || item.category === "WSD";
     });
   });
 
@@ -25,9 +23,25 @@ export const getDailyWeather: GeolocationProps = async (data) => {
     });
   });
 
-  res.push(nowTemperatures);
-  res.push(newSky[0]);
-  return res;
+  const minMax: Promise<AxiosResponse<resultDailyTemperatureProps>> = await axios.get(`http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst?serviceKey=${KOREA_WEATHER_API_KEY}&numOfRows=40&pageNo=1&dataType=json&base_date=${BASE_DATE}&base_time=0200&nx=${nx ? nx : 60}&ny=${ny ? ny : 127}`).then((res) => {
+    const result = res.data.response.body.items.item;
+    return result.filter((item: resultDailyTemperatureProps) => {
+      return item.category === "TMX" || item.category === "TMN";
+    });
+  });
+
+  const currentWeather = {
+    reh: nowTemperatures[0].obsrValue,
+    rn1: nowTemperatures[1].obsrValue,
+    t1h: nowTemperatures[2].obsrValue,
+    vec: nowTemperatures[3].obsrValue,
+    wsd: nowTemperatures[4].obsrValue,
+    sky: newSky[0].fcstValue,
+    tmn: minMax[0].fcstValue,
+    tmx: minMax[1].fcstValue,
+  };
+
+  return currentWeather;
 };
 
 export const getMaxMinTemperature: GeolocationProps = async (data) => {
