0a98f94719629a1cc5080575f70bf9407f8049a7
jsg3121 <xodm95@gmail.com>
2021-08-29 12:54:45 +0900

    FIX(build) :server build 버전 업데이트

--- build/common/src/date.d.ts
+++ build/common/src/date.d.ts
@@ -3,3 +3,4 @@ export declare const defaultTime: () => string;
 export declare const changDateFormMiniDust: () => string;
 export declare const changDateFormThreeHoursTime: () => string;
 export declare const checkWeeklyDate: () => string;
+export declare const calcDate: () => string;
