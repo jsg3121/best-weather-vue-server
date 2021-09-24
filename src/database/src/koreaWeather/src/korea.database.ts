feee498066313d05c164d9b8c07c89934a03f24f
jsg3121 <xodm95@gmail.com>
2021-09-25 08:50:34 +0900

    불필요한 코드, 폴더 제거

--- src/database/src/koreaWeather/src/korea.database.ts
+++ src/database/src/koreaWeather/src/korea.database.ts
@@ -1,24 +1,3 @@
 import { PrismaClient } from "@prisma/client";
 
-export const weeklyWeatherKoreaDatabase = (data) => {
-  const prisma = new PrismaClient();
-  const main = async (data) => {
-    return await prisma.weatherKoWeekly.create({ data: data }).then((res) => {
-      if (res !== null) {
-        return false;
-      }
-      return true;
-    });
-  };
-
-  return main(data)
-    .then((res) => {
-      return res;
-    })
-    .catch((e) => {
-      return e;
-    })
-    .finally(async () => {
-      await prisma.$disconnect();
-    });
-};
+export const weeklyWeatherKoreaDatabase = (data) => {};
